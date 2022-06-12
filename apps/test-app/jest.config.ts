b807141cc62e4d37ea83480737efd24280b5dbba
Bombillazo <hector@hyperion.finance>
2022-06-11 22:17:58 -0400

    Setup nx expo repo

---
+++ apps/test-app/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'test-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/test-app',
+};
